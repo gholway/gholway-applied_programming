cb076e3ae1d29cd1eb9c15c54be32bd5e212fb27
gholway <131705574+gholway@users.noreply.github.com>
2026-01-09 11:12:19 -0700

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+# Written by: Gavin Howlay
+
+# Overview
+
+# This is a simple "Hello, World!" program written in Python.
+
+# This was written to refresh myself on the print function of Python.
+
+# Development Environment
+
+# This program was made using Python in visual studio code.
+
+# This program can be found at https://github.com/gholway/gholway-applied_programming as HelloWorld.py
+
+print("Hello, World!") # This line prints "Hello, World!" to the console.
